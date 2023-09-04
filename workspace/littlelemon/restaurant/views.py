2890506f7c0a0c4a86ebcae50dd76034a2d28d31
Esmaael <esmaaeelaljrf@gmail.com>
2023-09-04 11:01:22 +0300

    Auth_Token_Part

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
 from .serializers import MenuSerializer, BookingSerializer
 from .models import Menu, Booking
+from django.http import HttpResponse
 from rest_framework import generics,viewsets
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes, api_view
 
 # Create your views here.
-# Menu view
+# @api_view()
+# @permission_classes([IsAuthenticated])
+# def test(request):
+#     return HttpResponse("Tessst")
+
+# # Menu view
 class MenuItemView (generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
     
 class SingleMenuItemView (generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
@@ -16,6 +23,8 @@ class SingleMenuItemView (generics.RetrieveUpdateAPIView, generics.DestroyAPIVie
 
 
 # Booking view
+
 class BookingViewSet (viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
